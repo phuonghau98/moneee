ed113d785221f28387c0244e35f6680a489886d7
Phuong Hau <hvphuong98@gmail.com>
2019-01-04 14:23:59 +0700

    use interceptor instead of middleware due to Nestjs v4.6

--- src/modules/upload/module.ts
+++ src/modules/upload/module.ts
@@ -3,7 +3,6 @@ import { UploadController } from './controller'
 import { UploadService } from './service'
 import { ConfigModule } from 'dist/src/config/module'
 import { MulterConfigService } from '../../config/multerService'
-import { MulterMiddleware } from '../../common/middlewares/multer'
 
 @Module({
   imports: [
@@ -15,13 +14,4 @@ import { MulterMiddleware } from '../../common/middlewares/multer'
   controllers: [ UploadController ],
   providers: [ UploadService ]
 })
-export class UploadModule implements NestModule{ 
-  configure( consumer: MiddlewareConsumer) {
-    consumer
-      .apply(MulterMiddleware)
-      .with([{ name: 'profile_image' }])
-      .forRoutes(
-        { path: '/profileImage', method: RequestMethod.POST }
-      )
-  }
-}
+export class UploadModule { }
