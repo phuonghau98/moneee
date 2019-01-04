9d4b5e163d8fbd0527a69d22a5272ae4c375a83c
Phuong Hau <hvphuong98@gmail.com>
2019-01-04 14:14:19 +0700

    Created multer middleware, then applied it to upload module

--- src/modules/upload/module.ts
+++ src/modules/upload/module.ts
@@ -1,8 +1,9 @@
-import { Module, MulterModule } from '@nestjs/common'
+import { Module, MulterModule, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common'
 import { UploadController } from './controller'
 import { UploadService } from './service'
 import { ConfigModule } from 'dist/src/config/module'
 import { MulterConfigService } from '../../config/multerService'
+import { MulterMiddleware } from '../../common/middlewares/multer'
 
 @Module({
   imports: [
@@ -14,4 +15,13 @@ import { MulterConfigService } from '../../config/multerService'
   controllers: [ UploadController ],
   providers: [ UploadService ]
 })
-export class UploadModule { }
+export class UploadModule implements NestModule{ 
+  configure( consumer: MiddlewareConsumer) {
+    consumer
+      .apply(MulterMiddleware)
+      .with([{ name: 'profile_image' }])
+      .forRoutes(
+        { path: '/profileImage', method: RequestMethod.POST }
+      )
+  }
+}
